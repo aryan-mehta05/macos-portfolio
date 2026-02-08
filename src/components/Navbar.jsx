47155e9987ad97ea1063a110312911efaca5d77f
Aryan Mehta <aryannmehta@gmail.com>
2026-02-07 19:34:52 -0500

    finish navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
-import { navLinks } from "#constants";
+import dayjs from "dayjs";
+
+import { navIcons, navLinks } from "#constants";
 
 const Navbar = () => {
   return (
@@ -14,6 +16,18 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} alt={`icon-${id}`} className="icon-hover" />
+            </li>
+          ))}
+        </ul>
+
+        <time>{dayjs().format('ddd MMM D h:mm A')}</time>
       </div>
     </nav>
   )
