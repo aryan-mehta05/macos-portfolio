5ac923496170c5f51a2573334c8b200dbd5e7d86
Aryan Mehta <aryannmehta@gmail.com>
2026-02-10 06:43:24 -0500

    implement finder window with location store

---
+++ src/windows/Finder.jsx
@@ -0,0 +1,75 @@
+import clsx from "clsx";
+import { Search } from "lucide-react";
+
+import { locations } from "#constants";
+import { WindowControls } from "#components";
+import WindowWrapper from "#hoc/WindowWrapper";
+import useWindowStore from "#store/window";
+import useLocationStore from "#store/location";
+
+const Finder = () => {
+  const { openWindow } = useWindowStore();
+  const { activeLocation, setActiveLocation } = useLocationStore();
+
+  const openItem = item => {
+    if (item.fileType === 'pdf') return openWindow('resume');
+    if (item.kind === 'folder') return setActiveLocation(item);
+
+    if (['fig', 'url'].includes(item.fileType) && item.href) return window.open(item.href, "_blank");
+
+    openWindow(`${item.fileType}${item.kind}`, item);
+  };
+
+  const renderList = (name, items) => (
+    <>
+      <h3>{name}</h3>
+
+      <ul>
+        {items.map((item) => (
+          <li
+            key={item.id}
+            className={clsx(item.id === activeLocation.id ? "active" : "not-active")}
+            onClick={() => setActiveLocation(item)}
+          >
+            <img src={item.icon} className="w-4" alt={item.name} />
+            <p className="text-sm font-medium truncate">{item.name}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+  
+  return (
+    <>
+      <div id="window-header">
+        <WindowControls target="finder" />
+        <Search className="icon" />
+      </div>
+
+      <div className="bg-white flex h-full">
+        <div className="sidebar">
+          {renderList("Favorites", Object.values(locations))}
+          {renderList("Projects", locations.work.children)}
+        </div>
+
+        <ul className="content">
+          {activeLocation?.children.map((item) => (
+            <li
+              key={item.id}
+              className={item.position}
+              onClick={() => openItem(item)}
+            >
+              <img src={item.icon} alt={item.name} />
+              <p>{item.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    </>
+  );
+};
+
+const FinderWindow = WindowWrapper(Finder, 'finder');
+
+export default FinderWindow;
