2eb40da7e29b2029a6fd0f5be9de150ea34956f6
Aryan Mehta <aryannmehta@gmail.com>
2026-02-10 07:59:08 -0500

    implement home screen folders

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Dock from "#components/Dock";
 import Navbar from "#components/Navbar";
 import Welcome from "#components/Welcome";
 import WindowControls from "#components/WindowControls";
+import Home from "#components/Home";
 
-export { Navbar, Welcome, Dock, WindowControls };
+export { Navbar, Welcome, Dock, WindowControls, Home };
