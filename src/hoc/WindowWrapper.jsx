f9c4600d88dca85471503fa48a7559537a1423e2
Aryan Mehta <aryannmehta@gmail.com>
2026-02-10 04:09:15 -0500

    create window-wrapper hoc, terminal window ui

---
+++ src/hoc/WindowWrapper.jsx
@@ -0,0 +1,28 @@
+import { useRef } from "react";
+
+import useWindowStore from "#store/window";
+
+const WindowWrapper = (Component, windowKey) => {
+  const Wrapped = (props) => {
+    const { focusWindow, windows } = useWindowStore();
+    const { isOpen, zIndex } = windows[windowKey];
+    const ref = useRef(null);
+
+    return (
+      <section
+        id={windowKey}
+        ref={ref}
+        style={{ zIndex }}
+        className="absolute"
+      >
+        <Component {...props} />
+      </section>
+    );
+  };
+
+  Wrapped.displayName = `WindowWrapper(${Component.displayName || Component.name || "Component"})`;
+  
+  return Wrapped;
+};
+
+export default WindowWrapper;
